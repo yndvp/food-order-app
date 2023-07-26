77920868b3f8562d365b75247b259ca5c40846e9
yndvp <58580623+yndvp@users.noreply.github.com>
2023-07-25 21:12:26 -0400

    style: remove rebundant code

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -32,14 +32,14 @@ const DUMMY_MEALS = [
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
-      id={meal.id}
       key={meal.id}
+      id={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
     />
   ));
-  console.log(mealsList);
+
   return (
     <section className={styles.meals}>
       <Card>
